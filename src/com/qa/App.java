5ae8180fa5366dab5252933b64ed4c50153063a3
MrWalshyType2 <mwalsh@qa.com>
2023-06-02 14:45:31 +0100

    Updated Song class to use LocalDate for release date instead of LocalDateTime

--- src/com/qa/App.java
+++ src/com/qa/App.java
@@ -2,24 +2,22 @@ package com.qa;
 
 import com.qa.data.Song;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
 
 public class App {
 
     public static void main(String[] args) {
         Song song1 = new Song(
                 1, "Frog Prince", "Keane", 300,
-                LocalDateTime.of(2006, 6, 12, 0, 0, 0)
+                LocalDate.of(2006, 6, 12)
         );
         Song song2 = new Song(
                 2, "Black Summer", "Red Hot Chilli Peppers", 250,
-                LocalDateTime.of(2022, 2, 4, 0, 0, 0)
+                LocalDate.of(2022, 2, 4)
         );
         Song song3 = new Song(
                 2, "Con te partiro", "Andrea Boucelli", 250,
-                LocalDateTime.of(1990, 6, 1, 0, 0, 0)
+                LocalDate.of(1990, 6, 1)
         );
 
         System.out.println(song1);
--- src/com/qa/data/Song.java
+++ src/com/qa/data/Song.java
@@ -1,6 +1,6 @@
 package com.qa.data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Song {
@@ -9,19 +9,19 @@ public class Song {
     private String title;
     private String artist;
     private int runtime;
-    private LocalDateTime releaseDate;
+    private LocalDate releaseDate;
 
     public Song() {
     }
 
-    public Song(String title, String artist, int runtime, LocalDateTime releaseDate) {
+    public Song(String title, String artist, int runtime, LocalDate releaseDate) {
         this.title = title;
         this.artist = artist;
         this.runtime = runtime;
         this.releaseDate = releaseDate;
     }
 
-    public Song(int id, String title, String artist, int runtime, LocalDateTime releaseDate) {
+    public Song(int id, String title, String artist, int runtime, LocalDate releaseDate) {
         this.id = id;
         this.title = title;
         this.artist = artist;
@@ -61,11 +61,11 @@ public class Song {
         this.runtime = runtime;
     }
 
-    public LocalDateTime getReleaseDate() {
+    public LocalDate getReleaseDate() {
         return releaseDate;
     }
 
-    public void setReleaseDate(LocalDateTime releaseDate) {
+    public void setReleaseDate(LocalDate releaseDate) {
         this.releaseDate = releaseDate;
     }
 
